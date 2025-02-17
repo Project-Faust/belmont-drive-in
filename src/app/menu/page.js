1434dcbc4b795a5c11f086a31c265d5eea554211
Project-Faust <pentecost.cody.lane@gmail.com>
2025-02-17 11:49:14 -0500

    format section; changed heading layout

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,5 +1,11 @@
 export default function Menu() {
-    <div>
-        <h1>Menu</h1>
-    </div>
+    return (
+        <section className="min-h-screen flex flex-col items-center justify-start py-12">
+            <div className="text-center mb-12">
+                <h2 className="text-3xl font-bold">
+                    <strong>Menu</strong>
+                </h2>
+            </div>
+        </section>
+    )
 }
