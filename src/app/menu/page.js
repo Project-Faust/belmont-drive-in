4ba86e4ec9d9cf47c0bb335e44186a3244758b89
Project-Faust <pentecost.cody.lane@gmail.com>
2025-02-17 13:28:14 -0500

    corrected img import; header layout/content update

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,11 +1,36 @@
+// import menuPoster from "../../../public/images/menu-downscaled.webp";
+
 export default function Menu() {
     return (
         <section className="min-h-screen flex flex-col items-center justify-start py-12">
+
             <div className="text-center mb-12">
-                <h2 className="text-3xl font-bold">
+                <h2 className="text-3xl mb-5">
                     <strong>Menu</strong>
                 </h2>
+                <h3 className="text-2xl mb-5">Allergy Warnings & Diet Considerations</h3>
             </div>
+
+            <section className="flex">
+
+                <div className="px-12 mb-5">
+                    <img
+                        className=""
+                        src="/images/menu-downscaled.webp"
+                        alt="Menu"
+                    />
+                </div>
+
+                <div className="px-12 mb-5">
+                    <img
+                        className=""
+                        src="/images/2024-food-allergies.webp"
+                        alt="Food Allergies"
+                    />
+                </div>
+
+            </section>
+
         </section>
     )
 }
--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -1,7 +1,21 @@
 export default function Rules() {
     return (
-        <div>
-            <h1>Rules and Regulations</h1>
-        </div>
+        <section className="min-h-screen flex flex-col items-center justify-start py-12">
+
+            <div className="text-center mb-12">
+                <h2 className="text-3xl mb-5">
+                    <strong>Rules and Regulations</strong>
+                </h2>
+                <h3 className="text-2xl mb-5">Please take a moment to familiarize yourself with how we operate.</h3>
+            </div>
+
+            <div>
+                <img
+                    src="/images/2024-rules.webp"
+                    alt="Rules & Regulations"
+                />
+            </div>
+
+        </section>
     )
 }
