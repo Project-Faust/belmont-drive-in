33260c648511b9ac1cf01091ea3906eb682f33ee
Project-Faust <pentecost.cody.lane@gmail.com>
2025-02-17 20:16:30 -0500

    updated layout for map embed

--- src/app/page.js
+++ src/app/page.js
@@ -28,31 +28,35 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="flex justify-center mx-2">
-        <ul className="text-xl text-center mx-2 mt-5 mb-2">
-          <li className="">
-            We are open seasonally on <strong>Friday</strong> and <strong>Saturday</strong> nights from April to the first week in December.
-            <br />
-            You can find us at <a className="text-blue-600 hover:underline">314 McAdenville Rd, Belmont NC 28120</a>.
-          </li>
-          <br />
-          <li>
-            We are a <strong>cash only</strong> business. Card and tap to pay <strong>are not accepted</strong> at this time.
-          </li>
-          <br />
-          <li className="">
-            We have one screen and <strong>we play two movies each</strong> night we're open. <br />We always play <strong>one movie after another</strong> with a brief intermission in between.
-          </li>
-          <br />
-          <li>
-            Our <strong>first movie starts</strong> after the sun dips below the horizon. <br /> Our earliest movie start time will be <strong>no sooner than 7:30PM</strong>.
-          </li>
-        </ul>
-      </section>
+      <div className="flex justify-evenly">
 
-      <section>
-        <MapsEmbed></MapsEmbed>
-      </section>
+        <main className="flex justify-center mx-2">
+          <ul className="text-xl text-center mx-2 mt-5 mb-2">
+            <li className="">
+              We are open seasonally on <strong>Friday</strong> and <strong>Saturday</strong> nights from April to the first week in December.
+              <br />
+              You can find us at <a className="text-blue-600 hover:underline" href="https://www.google.com/maps/dir//Belmont+Drive-In+314+McAdenville+Rd+Belmont,+NC+28012/@35.256076,-81.0586444,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8856bdea1cb81aa9:0xdda9335f9ba8dbe9!2m2!1d-81.0586444!2d35.256076?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoJLDEwMjExMjMzSAFQAw%3D%3D" target="_blank" rel="noreferrer">314 McAdenville Rd, Belmont NC 28120</a>.
+            </li>
+            <br />
+            <li>
+              We are a <strong>cash only</strong> business. Card and tap to pay <strong>are not accepted</strong> at this time.
+            </li>
+            <br />
+            <li className="">
+              We have one screen and <strong>we play two movies each</strong> night we're open. <br />We always play <strong>one movie after another</strong> with a brief intermission in between.
+            </li>
+            <br />
+            <li>
+              Our <strong>first movie starts</strong> after the sun dips below the horizon. <br /> Our earliest movie start time will be <strong>no sooner than 7:30PM</strong>.
+            </li>
+          </ul>
+        </main>
+
+        <div className="flex">
+          <MapsEmbed></MapsEmbed>
+        </div>
+
+      </div>
 
     </div>
 
