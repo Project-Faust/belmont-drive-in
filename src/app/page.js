266261e4cb4b2844b1c1942d416230b6ad3e71fd
Project-Faust <pentecost.cody.lane@gmail.com>
2025-02-17 21:25:45 -0500

    refactored to move html content to individual components

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,27 @@
-import MapsEmbed from "./components/google/mapsEmbed";
+import MapsEmbed from "./components/google/MapsEmbed";
+import AlertInfo from "./components/ui/AlertInfo";
+import BusinessDetails from "./components/BusinessDetails";
 
 export default function Home() {
   return (
-
-    <div>
-
-      <header className="text-center mx-5 my-12">
+    <div className="mx-5 my-12">
+      <header className="text-center">
         <h1 className="text-2xl">Home of Your Affordable Family Entertainment Center</h1>
       </header>
 
-      <section className="flex justify-center mb-5">
-        <div>
-          <div role="alert" className="alert alert-info inline-flex items-center justify-center mx-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="h-6 w-6 shrink-0 stroke-current">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
-            <span>We are currently planning to open for the 2025 season in April. We will continue to post updates.</span>
-          </div>
-        </div>
+      <section className="flex justify-center my-5">
+        <AlertInfo />
       </section>
 
-      <div className="flex justify-evenly">
-
-        <main className="flex justify-center mx-2">
-          <ul className="text-xl text-center mx-2 mt-5 mb-2">
-            <li className="">
-              We are open seasonally on <strong>Friday</strong> and <strong>Saturday</strong> nights from April to the first week in December.
-              <br />
-              You can find us at <a className="text-blue-600 hover:underline" href="https://www.google.com/maps/dir//Belmont+Drive-In+314+McAdenville+Rd+Belmont,+NC+28012/@35.256076,-81.0586444,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8856bdea1cb81aa9:0xdda9335f9ba8dbe9!2m2!1d-81.0586444!2d35.256076?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoJLDEwMjExMjMzSAFQAw%3D%3D" target="_blank" rel="noreferrer">314 McAdenville Rd, Belmont NC 28120</a>.
-            </li>
-            <br />
-            <li>
-              We are a <strong>cash only</strong> business. Card and tap to pay <strong>are not accepted</strong> at this time.
-            </li>
-            <br />
-            <li className="">
-              We have one screen and <strong>we play two movies each</strong> night we're open. <br />We always play <strong>one movie after another</strong> with a brief intermission in between.
-            </li>
-            <br />
-            <li>
-              Our <strong>first movie starts</strong> after the sun dips below the horizon. <br /> Our earliest movie start time will be <strong>no sooner than 7:30PM</strong>.
-            </li>
-          </ul>
+      <div className="flex flex-col md:flex-row justify-evenly items-center">
+        <main className="mx-2">
+          <BusinessDetails />
         </main>
 
-        <div className="flex">
-          <MapsEmbed></MapsEmbed>
-        </div>
-
+        <section className="flex">
+          <MapsEmbed />
+        </section>
       </div>
-
     </div>
-
   );
 }
