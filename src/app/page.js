1d34545737c63cb9ce85b1cb90f8cda4d86c0d28
Project-Faust <pentecost.cody.lane@gmail.com>
2025-02-17 22:18:21 -0500

    fix import after file name/path change

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import AlertInfo from "./components/ui/AlertInfo";
 import BusinessDetails from "./components/BusinessDetails";
-import MapsEmbed from "./components/google/MapsEmbed";
+import MapsEmbed from "./components/google/MapEmbed";
 
 export default function Home() {
   return (
